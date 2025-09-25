b70290a3f349dcdf0407ba322ce8945e55889ff8
T Ol <31147296+tunprimus@users.noreply.github.com>
2025-09-25 10:16:59 +0100

    feat: Add functions to query databases

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import httpimport
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -22,24 +23,38 @@ from pprint import pprint
 
 load_dotenv(".env")
 
-try:
-    sys.path.append(realpath(expanduser("../../../snippets_n_gists/data_utils")))
-except Exception as exc:
-    sys.path.append(realpath(expanduser("../../../snippets_n_gists/data_utils")))
+# Define constants
+GOLDEN_RATIO = 1.618033989
+FIG_WIDTH = 12
+FIG_HEIGHT = FIG_WIDTH / GOLDEN_RATIO
+FIG_SIZE = (FIG_WIDTH, FIG_HEIGHT)
+FIG_DPI = 72
+RANDOM_SAMPLE_SIZE = 13
+RANDOM_SEED = 42
+ALPHA_VALUE = 0.05
+
+# Set the Git repo URL and the name of the modules to import
+gitlab_repo_username = "tunprimus"
+gitlab_repo_target = "snippets_n_gists"
+gitlab_repo_branch = "main"
+module_names = ["load_sqlite_tables_without_knowing_names", "multi_rdbm_executor", "standardise_column_names", "timing_function"]
 
 try:
-    sys.path.append(
-    realpath(expanduser("../../../snippets_n_gists/data_utils/data_processing_utils"))
-)
-except Exception as exc:
-    sys.path.append(
-    realpath(expanduser("../../../snippets_n_gists/data_utils/data_processing_utils"))
-)
+    with httpimport.gitlab_repo(gitlab_repo_username, gitlab_repo_target, ref=gitlab_repo_branch):
+        from data_utils.load_sqlite_tables_without_knowing_names import read_all_sqlite_tables
+        from data_utils.multi_rdbm_executor import create_sql_connection, execute_on_database
+        from data_utils.standardise_column_names import standardise_column_names
+        from data_utils.timing_function import timing_function
+except (ModuleNotFoundError, ImportError):
+    try:
+        sys.path.append(realpath(expanduser("../../../../snippets_n_gists/data_utils")))
 
-from load_sqlite_tables_without_knowing_names import read_all_sqlite_tables
-from multi_rdbm_executor import create_sql_connection, execute_on_database
-from standardise_column_names import standardise_column_names
-from timing_function import timing_function
+        from load_sqlite_tables_without_knowing_names import read_all_sqlite_tables
+        from multi_rdbm_executor import create_sql_connection, execute_on_database
+        from standardise_column_names import standardise_column_names
+        from timing_function import timing_function
+    except (ModuleNotFoundError, ImportError):
+        raise("Failed to import module from both online and offline sources.")
 
 warnings.filterwarnings("ignore")
 
@@ -106,6 +121,7 @@ def display_table_exec_result(
     db_port=None,
     db_name=None,
     query_to_exec=None,
+    return_df=False,
 ):
     """
     Displays the result of a query on a given table.
@@ -130,6 +146,8 @@ def display_table_exec_result(
         The name of the database. Defaults to None.
     query_to_exec : str, optional
         The query to execute on the database. Defaults to None.
+    return_df : boolean, optional
+        If True, returns the DataFrame after displaying it.
 
     Returns
     -------
@@ -154,7 +172,5 @@ def display_table_exec_result(
         print("No data returned!")
         return
     display(df_temp)
-
-
-
-
+    if return_df:
+        return df_temp
